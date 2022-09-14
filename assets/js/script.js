0f5c2e377adc0cc9cc88c8acd307458afe693fb6
Joon Park <jsp220@gmail.com>
2022-09-14 14:29:52 -0700

    Add a search bar with fuctionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,26 +12,25 @@ function DarkMode() {
 //  Crypto API
 
 // for testing init
-var abc = ["bitcoin", "ethereum"];
-localStorage.setItem("cryptoList", JSON.stringify(abc)); //
-
+var favCrypto = [];
 
 function init() {
     // Retrieve favorite assets data from local storage
     var favCryp = localStorage.getItem("cryptoList");
     if (!favCryp) {
-        var favCrypto = [];
+        favCrypto = [];
     } else {
         favCrypto = JSON.parse(favCryp);
         favCrypto.splice(10); // limit to 10 crypto?
     }
-    console.log(favCrypto);
+    // console.log(favCrypto);
     favCryptoApi(favCrypto);
 }
 
-function favCryptoApi(favCrypto) {
+async function favCryptoApi(favCrypto) {
     for (i in favCrypto) {
-        cryptoApi(favCrypto[i]);
+        var data = await cryptoApi(favCrypto[i]);
+        appendFave(data);
     }
 }
 
@@ -39,14 +38,23 @@ function favCryptoApi(favCrypto) {
 async function cryptoApi(cryptoId) {
     var cryptoApiUrl = "https://api.coingecko.com/api/v3/coins/" + cryptoId;
     
-    console.log(cryptoApiUrl);
-
     var data = await fetch(cryptoApiUrl)
         .then(function(response) {
             return response.json();
         });
-    
-    console.log(data);
+    return data;
+}
+
+function appendFave (data) {
+    var divEl = $("<div class='test'>");
+    var symb = data.symbol;
+    var symb = symb.toUpperCase();
+    var price = data.market_data.current_price.usd;
+    var priceChg = data.market_data.price_change_24h;
+    var priceChg = priceChg.toFixed(2);
+    var priceChgPcnt = (priceChg/price*100).toFixed(2);
+    divEl.text(`${symb} $${price} $${priceChg} (${priceChgPcnt}%)`);
+    $(".fav-list").append(divEl);
 }
 
 async function searchTermToId(term) {
@@ -55,7 +63,6 @@ async function searchTermToId(term) {
             return response.json();
         })
     term = term.toLowerCase();
-    console.log(term);
     for (i in data) {
         if (term == data[i].id || term == data[i].name || term == data[i].symbol) {
             cryptoId = data[i].id;
@@ -69,28 +76,49 @@ async function searchTermToId(term) {
     return cryptoId;
 }
 
-async function search (cryptoSearchTerm) {
-    var cryptoId = await searchTermToId(cryptoSearchTerm);
-    cryptoApi(cryptoId);
-}
+
 
 
 // favCryptoApi();
 
 // search bar 
-// $("#add").on("click", async function() {
-//     var cryptoSearchTerm = $(this).siblings("#search-term").val();
-    
-//     if (!cryptoSearchTerm) {
-//         return;
-//     }
-    
-//     var cryptoId = await searchTermToId(cryptoSearchTerm);
-//     // cryptoApi(cryptoId);
 
-// })
-
+// allows the user to hit enter key to search instead of clicking the search button
 
 init();
-var cryptoSearchTerm = "ADA";
-search(cryptoSearchTerm);
+
+$("#search-term").keypress(function (a) {
+    var key = a.which;
+    if (key == 13) {
+        $("#add").click();
+    }
+})
+
+$("#add").on("click", async function() {
+    var cryptoSearchTerm = $(this).siblings("#search-term").val();
+    
+    if (!cryptoSearchTerm) {
+        return;
+    }
+    
+    var cryptoId = await searchTermToId(cryptoSearchTerm);
+    var data = await cryptoApi(cryptoId);
+    appendFave(data);
+    // var favCryp = localStorage.getItem("cryptoList");
+    // if (!favCryp) {
+    //     favCrypto = [];
+    // } else {
+    //     favCrypto = JSON.parse(favCryp);
+    // }
+    favCrypto.push(cryptoId);
+    localStorage.setItem("cryptoList", JSON.stringify(favCrypto));
+});
+
+// test search term
+// var cryptoSearchTerm = "ADA";
+// search(cryptoSearchTerm);
+
+// async function search (cryptoSearchTerm) {
+//     var cryptoId = await searchTermToId(cryptoSearchTerm);
+//     cryptoApi(cryptoId);
+// }
