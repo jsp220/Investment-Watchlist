d494e4f49ef60ee3cd22f50f423040dc643ae1c6
BrianZoulko <bzoulko@gmail.com>
2022-09-14 12:16:19 -0700

    Updated CryptoNewsAPI comments

--- assets/js/newsapi.js
+++ assets/js/newsapi.js
@@ -8,9 +8,14 @@
     ============
     09/14/2022 Brian Zoulko    Created news api call module as a function.
 */
-const cNEWS_URL = 'https://google-news.p.rapidapi.com/v1/top_headlines?lang=en&country=US'
+const cGOOGLE_NEWS_URL = 'https://google-news.p.rapidapi.com/v1/top_headlines?lang=en&country=US'
+const cCRYPTO_NEWS_URL = 'https://crypto-news-live3.p.rapidapi.com/news'
 
-function getNews(strKey) {
+
+/*
+    GOOGLE News API.
+*/
+function getGoogleNews() {
 
     // Google News API options.
     const options = {
@@ -32,10 +37,42 @@ function getNews(strKey) {
 
         console.log(data);
 
-    }).catch(err){
+    }).catch(err => console.error(err));
 
-        alert(err);
-        
+}
+
+
+/*
+    CRYPTO News API.
+*/
+function getCryptoNews() {
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4cd32e560emsh5d716242c652ea5p114f81jsne9cf0d28233d',
+            'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+        }
     };
+
+
+    // ---------------------------------------------------------
+    // F E T C H  News information using RapidAPI w/crypto-news.
+    // ---------------------------------------------------------
+    fetch(cCRYPTO_NEWS_URL, options).then(function(response){
+
+        return (response.json());
+
+    }).then(function(data){
+
+        console.log(data.length);
+        for (let x = 0; x < data.length; x++) {
+            if (data[x].title.search("Apple")) {
+                console.log(data[x].title);
+            }
+        }
+
+    }).catch(err => console.error(err));
+
 
 }
